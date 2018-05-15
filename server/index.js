bb1ca31f6db505c04ecb4180dad792df6a0622a4
Conner McKenna <cmckenna@ellisdon.com>
2018-05-15 13:30:52 -0400

    adding config file for server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+var config = require('./config');
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname +'./../../')); //serves the index.html
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+app.use(express.static(__dirname +'./../')); //serves the index.html
+app.listen(config.port); //listens on port 3000 -> http://localhost:3000/
+
+console.log(`listening on port ${config.port}`)
