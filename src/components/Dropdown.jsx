f078d028cca77b0ba0059b73fe3b75ca989f1849
Conner McKenna <cmckenna@ellisdon.com>
2018-05-24 14:30:00 -0400

    fixing styles for dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,13 +6,22 @@ class Dropdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeElement: null
+      activeElement: null,
+      activeIndex: null
     }
   }
   
   handleClick = (item) => {
     this.setState({activeElement: item});
     this.props.onSelect(item);
+  }
+
+  onMouseEnter = (i) => {
+    this.setState({activeIndex: i});
+  }
+  
+  onMouseLeave = () => {
+    this.setState({activeIndex: null});
   }
 
   render() {
@@ -22,20 +31,26 @@ class Dropdown extends React.Component {
           <p className="dropdown__header--text">Please select a role.</p>
         </div>
         <div className="dropdown__list">
-          <ul>
-            {this.props.listItems.map(item => 
-              <li 
-                value={item}
-                key={`list__${item}`}
-                onClick={() => {this.handleClick(item)}}
-                className={cx({"dropdown__list--active": this.state.activeElement === item})}
-              >
-              <p className="dropdown__list--text">
-                {item}
-              </p>
-              </li>
-            )}
-          </ul>
+          {this.props.listItems.map((item, i) =>   
+            <div 
+              key={item} 
+              className="dropdown__list-item"
+              onClick={() => this.handleClick(item)} 
+              onMouseEnter={() => this.onMouseEnter(i)} 
+              onMouseLeave={() => this.onMouseLeave(i)}
+            >
+              <div className="dropdown__list-item__bullet">
+                <svg height="20" width="20">
+                  <circle cx="10" cy="11" r="4" className={cx("dropdown__list-item__bullet__circle", {
+                    "dropdown__list-item__bullet__circle--solid": this.state.activeIndex === i || this.state.activeElement === item
+                  })}/>
+                </svg>
+              </div>
+              <div className="dropdown__list-item__content">
+                <p>{item}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
